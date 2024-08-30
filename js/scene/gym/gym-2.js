dd8d22ba9fa630ec7cf73d07040c965387adecf6
lhx <15935985812lhx@gmail.com>
2024-08-30 20:26:17 +0800

    1

--- js/scene/gym/gym-2.js
+++ js/scene/gym/gym-2.js
@@ -2,17 +2,17 @@ var c =
 {
     "type": 1,
 
-    "nextpage": "../../gym/gym-2-2.html",
+    "nextpage": "../../gameindex.html",
 
-    "steps": 2,
+    "steps": 1,
 
-    "name": ["教官","白特"],
+    "name": ["靶场管理员"],
 
-    "texts": ["先做一下简单的热身，然后绕着场馆跑五圈！之后我们再进行专项教学！","呼——呼——呼，好累，不过感觉体能又变好了，还掌握了一下新的体育专项知识。"],
+    "texts": ["同学，要打靶吗？"],
 
-    "small": ["none","白特"],
+    "small": ["none"],
 
-    "big": ["教官","none"],
+    "big": ["靶场管理员"],
 
-    "bigpos": ["middle","middle"]
+    "bigpos": ["middle"]
 };
