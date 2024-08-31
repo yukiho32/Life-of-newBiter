4a175f4d6c5e7a2f191ca3fee2f6d903113a2b6d
lhx <15935985812lhx@gmail.com>
2024-08-31 19:18:41 +0800

    2

---
+++ js/scene/game/game-3.js
@@ -0,0 +1,18 @@
+var c =
+{
+    "type": 0,
+
+    "nextpage": "../../../mini_game/PacMan.html",
+
+    "steps": 1,
+
+    "name": ["白特"],
+
+    "texts": ["这个游戏厅可以玩最近大火的《吃豆人》诶，我想玩好久了，今天一定要进去玩个过瘾！"],
+
+    "small": ["白特"],
+
+    "big": ["none"],
+
+    "bigpos": ["middle"]
+};
