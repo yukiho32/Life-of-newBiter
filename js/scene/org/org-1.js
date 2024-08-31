103c8dfbdc002e93e6166c975c0fab6606519d98
lhx <15935985812lhx@gmail.com>
2024-08-31 19:00:15 +0800

    1

---
+++ js/scene/org/org-1.js
@@ -0,0 +1,18 @@
+var c =
+{
+    "type": 0,
+
+    "nextpage": "../../gameindex.html",
+
+    "steps": 1,
+
+    "name": ["白特"],
+
+    "texts": ["今天是进行社团例会的日子，社长对社团接下来一段时间的工作安排和活动组织进行了详细的规划。"],
+
+    "small": ["白特"],
+
+    "big": ["none"],
+
+    "bigpos": ["middle"]
+};
---
+++ js/scene/org/org-3.js
@@ -0,0 +1,18 @@
+var c =
+{
+    "type": 0,
+
+    "nextpage": "../../gameindex.html",
+
+    "steps": 1,
+
+    "name": ["白特"],
+
+    "texts": ["今天社里组织了社团活动，可是似乎没什么人来，唉，本来期盼了好久的活动."],
+
+    "small": ["白特"],
+
+    "big": ["none"],
+
+    "bigpos": ["middle"]
+};
